059a608479e90e4bd38ca8aea5e620552e3dafe9
Cristian-Nastase <cristianemail@duck.com>
2026-05-24 10:28:01 +0300

    added exceptions

---
+++ include/exceptions/GameExceptions.h
@@ -0,0 +1,45 @@
+#ifndef TRANSITTYCOON_GAMEEXCEPTIONS_H
+#define TRANSITTYCOON_GAMEEXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+class GameException : public runtime_error {
+public:
+    explicit GameException(const std::string& msg)
+        : runtime_error(msg) {}
+};
+
+class GameOverException : public GameException {
+public:
+    explicit GameOverException(const string& msg)
+        : GameException("[GAME OVER] " + msg) {}
+};
+
+class InsufficientFundsException : public GameException {
+public:
+    explicit InsufficientFundsException(int needed, int available)
+        : GameException("Fonduri insuficiente: ai nevoie de " + to_string(needed) + ", ai doar " + to_string(available)) {}
+};
+
+class InvalidUpgradeException : public GameException {
+public:
+    explicit InvalidUpgradeException(const string& msg)
+        : GameException("Upgrade invalid: " + msg) {}
+};
+
+class ConfigException : public GameException {
+public:
+    explicit ConfigException(const string& msg)
+        : GameException("Eroare configurare: " + msg) {}
+};
+
+class InvalidChoiceException : public GameException {
+public:
+    explicit InvalidChoiceException(const string& msg)
+        : GameException("Alegere invalida: " + msg) {}
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "../include/exceptions/GameExceptions.h"
 
 int main() {
+    try {
+        throw InsufficientFundsException(499, 200);
+    }
+    catch (GameException& e) {
+        cout<<e.what()<<'\n';
+    }
     std::cout << "Transport Tycoon - start\n";
     return 0;
 }
