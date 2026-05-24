3f658a570f7e95fd3747486d31f91d6e30cfab89
Cristian-Nastase <cristianemail@duck.com>
2026-05-24 10:08:28 +0300

    added data files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "TransitTycoon");
-    while (window.isOpen()) {
-        sf::Event e;
-        while (window.pollEvent(e))
-            if (e.type == sf::Event::Closed) window.close();
-        window.clear(sf::Color::Blue);
-        window.display();
-    }
+    std::cout << "Transport Tycoon - start\n";
+    return 0;
 }
